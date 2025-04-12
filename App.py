e941818832ab3df79143359f65def9bb607e338d
Murathan Arat <154649929+murathanarat@users.noreply.github.com>
2025-04-12 20:24:33 +0300

    Add files via upload

---
+++ App.py
@@ -0,0 +1,87 @@
+import os
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+#Chrome driver'ın olduğu dizini driver_path'a yazın
+driver_path = "C:/chromedriver-win64/chromedriver.exe"
+download_path = "C:/Users/Ornek/Desktop/Gazete_pdf"
+
+# Chrome'u başlat
+options = Options()
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service, options=options)
+
+# Ana sayfaya git
+base_url = "https://nek.istanbul.edu.tr/ekos/GAZETE/"
+driver.get(base_url)
+
+# Sayfanın yüklenmesini bekle (max 30 saniye)
+wait = WebDriverWait(driver, 60)
+wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "a.popular-category.h-40")))
+
+# Tüm gazete bağlantılarını al
+gazete_links = driver.find_elements(By.CSS_SELECTOR, "a.popular-category.h-40")
+gazete_link = [gazete.get_attribute("href") for gazete in gazete_links]
+
+# Her bir linke sırayla git
+for i, g_link in enumerate(gazete_link):
+    
+    # Eğer klasör yoksa oluştur
+    directory_path = os.path.join(download_path, g_link.split("=")[-1])
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+        print(directory_path , " klasör oluşturuldu")
+
+    print(f"[{i+1}] {g_link} sayfasına gidiliyor...")
+    driver.get(g_link)
+
+    # Butonları bekle ve tıkla
+    buttons = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "button.collapsible")))
+
+    for i in range(len(buttons)):
+
+        date_name_path = os.path.join(directory_path, buttons[i].text)
+        if not os.path.exists(date_name_path):
+            os.makedirs(date_name_path)
+            print(date_name_path , " klasör oluşturuldu")
+
+
+        print(f"[{i+1}] Butona tıklanıyor: {buttons[i].text}")
+        buttons[i].click()
+
+        # İçeriğin yüklenmesi için biraz bekle
+        wait.until(EC.presence_of_all_elements_located((By.LINK_TEXT, "Tıklayınız")))
+
+        # PDF bağlantılarını pdf_links e kayıt etme
+        pdf_links = driver.find_elements(By.LINK_TEXT, "Tıklayınız")
+        pdf_link = [link.get_attribute("href") for link in pdf_links]
+
+        # PDF'leri indir
+        for p_link in pdf_link:
+
+            file_name = p_link.split("/")[-1]
+            file_path = os.path.join(date_name_path, file_name)
+
+            # Eğer dosya zaten varsa, indirme işlemini atla
+            if os.path.exists(file_path):
+                print(f"{file_name} zaten mevcut, indirilmiyor.")
+                continue
+
+            # İndirme işlemi
+            print(f"{file_name} indiriliyor...")
+            response = requests.get(p_link, verify=False)
+
+            if response.status_code == 200:
+                with open(file_path, 'wb') as file:
+                    file.write(response.content)
+                print(f"Dosya başarıyla {file_name} olarak indirildi.")
+            else:
+                print(f"Dosya indirilirken bir hata oluştu: {file_name}")
+
+# Tarayıcıyı kapat
+driver.quit()
